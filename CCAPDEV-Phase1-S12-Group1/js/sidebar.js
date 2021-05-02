6a8092a19fea85f0a7ad2782cb378e083392e845
RobertJosephB <77220807+RobertJosephB@users.noreply.github.com>
2021-05-02 15:47:25 +0800

    Initial HTML+CSS+Navbar
    Not yet functional

---
+++ CCAPDEV-Phase1-S12-Group1/js/sidebar.js
@@ -0,0 +1,23 @@
+
+function openNav() {
+  document.getElementById("sidebar-functions").style.width = "200px";
+}
+
+
+function closeNav() {
+  document.getElementById("sidebar-functions").style.width = "0";
+}
+function searchBoxShow(){
+
+	document.getElementById("searchbox").style.visibility = "visible";
+
+}
+
+function searchBoxHide(){
+
+	document.getElementById("searchbox").style.visibility = "hidden";
+
+
+}
+	
+		 
